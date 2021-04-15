feb431acde350f09d440eba798ebf1316923b0b0
Aishanaya01 <aishanayashukla85@gmail.com>
2021-04-15 22:11:15 +0530

    FoodUi

--- component/Count.js
+++ component/Count.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { View , Text } from "react-native";
 const Count = (props, { data }) => {
     const[count , setCount] = useState(0)
     return (
         <View>
+        <Text>
         {props.data.count}
-            <button onClick={() => setCount(count + 1)}> </button><Ionicons name="add-circle-outline" size={20} style={{padding:4}} />
+        </Text>
+            <Text><button onClick={() => setCount(count + 1)}> </button><Ionicons name="add-circle-outline" size={20} style={{padding:4}}/>
             <button onClick={() => setCount(count - 1)}> </button><Ionicons name="remove-circle-outline" size={20} style={{padding:4}} />
+            </Text>
         </View>
 
     )
